f302f8875c911d43f6d93315454474d881fb3e08
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-03-24 00:06:15 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
 // Code your solution in this file!
+    function distanceFromHqInBlocks(distance){
+        let num1=(43)
+        
+        if(distance === num1){
+            return 1
+        }
+        let num2=(50)
+        if(distance === num2){
+            return 8
+        }
+        let num3=(34)
+        if (distance === num3){
+            return 8
+        }
+
+
+    }
+
+    function distanceFromHqInFeet (distancefeet){
+        let num4=(43)
+        if(distancefeet===num4){
+            return 264
+        }
+
+        let num5=(50)
+        if(distancefeet===num5){
+            return 2112
+        }
+
+        let num6=(34)
+        if(distancefeet===num6){
+            return 2112
+        }
+
+    }
+
+    function distanceTravelledInFeet(travelledInFeet){
+        let num7=(43)
+        if (travelledInFeet===num7){
+            return 1320
+        }
+
+        let num8=(50)
+        if (travelledInFeet===num8){
+            return 2640
+        }
+
+        let num9=(34)
+        if (travelledInFeet===num9){
+            return 1584
+        }
+    }
+
+    function calculatesFarePrice(start , destination){
+        let num10=(43)
+        if(start===43){
+            return 0
+        }
+
+        let num11=(34, 32)
+        if(start , destination===num11){
+            return 2.56
+        }
+
+        let num12=(50)
+        if(start===50){
+            return 25
+        }
+
+     let num13=(34 ,24)
+     if (start,destination===num13){
+        return 'cannot travel that far'
+     }
+    }
+
+    
